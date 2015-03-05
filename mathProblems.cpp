7c1b5dcf00f926eeec2619b13d41b060be7e2af1
Maggie Chang <maggie.bien.chang@sbcglobal.net>
2015-03-04 16:30:05 -0800

    Update mathProblems.cpp

--- mathProblems.cpp
+++ mathProblems.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main()
 {
-    string::level(); //excess elements in scalar initializer
-    string::name(); //cannot initialize a variable of type char with an Ivalue of type 'const char'
+    string level; //excess elements in scalar initializer
+    string name; //cannot initialize a variable of type char with an Ivalue of type 'const char'
     float a, b;
     cout << "input a level of difficulty : easy, medium, or super hard" << endl;
     cin >> level;
-    if ((level = "easy"))
+    if ((level == "easy"))
     {
         cout << "What is the shorter diagonal of a parallelogram that has sides of 3 and 8 and a base angle of 35°" << endl;
         cin >> a;
@@ -20,11 +20,11 @@ int main()
             cout << "You got it wrong. Sux for you. Come back tmrw" << endl;
         }
     }
-    else if ((level = "medium"))
+    else if ((level == "medium"))
     {
         cout << "Juan Pablo and Diana are racing to grab their dog chiquis. Juan Pablo is 500 ft from their dog and Diana is only 300 ft away. JP runs 8 ft persecond. Camila runs 6 ft per second, but she trips over a pile of dirt and starts 1 second later than Bert. Who gets to the ducky first?" << endl;
         cin >> name;
-        if (name = "Diana") //use of unceclared identifier 'Diana'
+        if (name == "Diana") //use of unceclared identifier 'Diana'
         {
             cout << "YOU GOT IT" << endl;
         }
@@ -33,7 +33,7 @@ int main()
             cout << "You lose and you suck at math. Try liberal arts" << endl;
         }
     }
-    else if ((level = "super hard"))
+    else if ((level == "super hard"))
     {
         cout << "an airplane is flying at a bearing of 200° at 500 mph. If the true course and bearing is 507 mph bearing 197°, what is the speed of the wind?" << endl;
         cin >> b;
